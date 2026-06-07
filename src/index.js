48fe1b2330edef646cb8974fae6488fea2d433de
Sampath Sitinamaluwa <sampath.sitinamaluwa@ifs.com>
2026-06-07 21:18:33 +0530

    Update to latest react/redux.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,12 +7,15 @@ import { CELLWIDTH, COLUMNS, ROWS } from './constants';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import createAutomataReducer from './automata';
+import { createRoot } from 'react-dom/client';
 
 window.data_doNotTime = true;
 const automataReducer = createAutomataReducer(COLUMNS, ROWS, CELLWIDTH);
 const store = createStore(automataReducer);
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
